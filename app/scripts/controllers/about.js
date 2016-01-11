62f54781b7df4b6d299cb2763e3c6e76f0efdb28
Nick Finley-Day <nickfday@gmail.com>
2016-01-11 19:15:56 +0000

    init commit. Added fontawesome and homepage markup

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name fitbugTestApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the fitbugTestApp
+ */
+angular.module('fitbugTestApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name fitbugTestApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the fitbugTestApp
+ */
+angular.module('fitbugTestApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
